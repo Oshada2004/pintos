14a68d40e56abfebd4b4af359df306847382db36
Oshada Dilvan <oshadajayasinghe5@gmail.com>
2026-08-19 15:11:04 +0530

    project 0 -3

--- src/threads/shell.c
+++ src/threads/shell.c
@@ -10,8 +10,7 @@
 #include "threads/pte.h"
 #include "threads/thread.h"
 
-void
-shell_run (void)
+void shell_run(void)
 {
   char buf[128];
   int idx;
@@ -23,74 +22,75 @@ shell_run (void)
   while (1)
     {
       printf ("CS2042> ");
-      idx = 0;
+      idx=0;
 
       while (1)
         {
-          char c = input_getc ();
+          char c=input_getc();
 
-          if (c == '\r' || c == '\n')
+          if(c=='\r' || c=='\n')
             {
               printf ("\n");
-              buf[idx] = '\0';
+              buf[idx]='\0';
               break;
             }
-          else if (c == '\b' || c == 127)
+          else if(c=='\b' || c==127)
             {
-              if (idx > 0)
+              if(idx > 0)
                 {
                   idx--;
                   printf ("\b \b");
                 }
             }
-          else if (idx < 127)
+          else if(idx < 127)
             {
-              buf[idx++] = c;
-              printf ("%c", c);
+              buf[idx++]=c;
+              printf ("%c",c);
             }
         }
 
-      if (strlen (buf) == 0)
+      if(strlen (buf)==0)
         continue;
 
-      if (!strcmp (buf, "whoami"))
+      if(!strcmp(buf,"whoami"))
         {
           printf ("Name: Oshada\nIndex: 240279C\n");
         }
-      else if (!strcmp (buf, "shutdown"))
+      else if(!strcmp(buf,"shutdown"))
         {
           printf ("Shutting down Pintos...\n");
-          shutdown_power_off ();
+          shutdown_power_off();
         }
-      else if (!strcmp (buf, "time"))
+      else if(!strcmp(buf,"time"))
         {
-          printf ("Seconds since Unix epoch: %lu\n",
-                  (unsigned long) rtc_get_time ());
+          unsigned total_seconds=rtc_get_time();
+          printf ("Seconds since Unix epoch: %u s\n",total_seconds);
+          printf ("Current Time: %u : %u : %u (UTC)\n",(total_seconds%86400)/3600,(total_seconds%3600)/60,total_seconds%60);
         }
-      else if (!strcmp (buf, "ram"))
+      else if(!strcmp(buf,"ram"))
         {
-          uint32_t total_ram_bytes = init_ram_pages * PGSIZE;
+          uint32_t total_ram_bytes=init_ram_pages*PGSIZE;
           printf ("RAM Pages: %u (%u KB / %u MB)\n",
                   init_ram_pages,
-                  total_ram_bytes / 1024,
-                  total_ram_bytes / (1024 * 1024));
+                  total_ram_bytes/1024,
+                  total_ram_bytes/(1024*1024));
         }
-      else if (!strcmp (buf, "thread"))
+      else if(!strcmp(buf,"thread"))
         {
-          thread_print_stats ();
+          thread_print_stats();
         }
-      else if (!strcmp (buf, "priority"))
+      else if(!strcmp(buf,"priority"))
         {
-          printf ("Current thread priority: %d\n", thread_get_priority ());
+          printf ("Current thread priority: %d\n",thread_get_priority());
         }
-      else if (!strcmp (buf, "exit"))
+      else if(!strcmp(buf,"exit"))
         {
           printf ("Exiting shell...\n");
           break;
         }
       else
         {
-          printf ("Unknown command: %s\n", buf);
+          printf ("Unknown command: %s\n",buf);
         }
     }
 }
